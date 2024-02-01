f27329ed4b751cd1e55884432b045ffb5331e39c
Julien Langlois <julien.langlois@autodesk.com>
2024-02-01 10:06:33 -0800

    Add log for proxy

--- python/tank/authentication/unified_login_flow2.py
+++ python/tank/authentication/unified_login_flow2.py
@@ -114,9 +114,15 @@ def process(
         MyRedirectHandler,
     ]
 
+    logger.debug("HTTP Proxy: {http_proxy}".format(http_proxy=http_proxy))
     if http_proxy:
         proxy_addr = _build_proxy_addr(http_proxy)
         sg_url_parsed = urllib.parse.urlparse(sg_url)
+
+        logger.debug("Set HTTP Proxy handler for {scheme} to {url}".format(
+            url=proxy_addr,
+            scheme=sg_url_parsed.scheme,
+        ))
 
         url_handlers.append(
             urllib.request.ProxyHandler(
